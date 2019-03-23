0d7bc8128c17e3f7776381fc111f6b6ec54bc655
alexis <alexis.heslouin@gmail.com>
2019-03-23 12:11:57 +0100

    ajout des models tag et story + début du design de projet

--- boards/app/models/project.js
+++ boards/app/models/project.js
@@ -5,5 +5,6 @@ export default DS.Model.extend({
   descriptif:DS.attr('string'),
   creationDate:DS.attr('date'),
   duoDate:DS.attr('date'),
-  owner:DS.belongsTo('developer')
+  owner:DS.belongsTo('developer'),
+  stories:DS.hasMany('story', {inverse:'project'})
 });
--- boards/app/router.js
+++ boards/app/router.js
@@ -18,6 +18,13 @@ Router.map(function() {
     this.route('edit', {path:'edit/:project_id'});
     this.route('delete', {path:'delete/:project_id'});
   });
+  this.route('project', {path:'project/:project_id'});
+
+  this.route('', function() {
+    this.route('story', function() {
+      this.route('new', {path:'new/:project_id'});
+    });
+  });
 });
 
 export default Router;
